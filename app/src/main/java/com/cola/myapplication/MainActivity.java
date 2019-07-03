0f44e185d82d9621adefb6be0f7c4ccb942826b8
coalwang <1974267123@qq,com>
2019-07-03 12:57:12 +0800

    Initial commit

---
+++ app/src/main/java/com/cola/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.cola.myapplication;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
